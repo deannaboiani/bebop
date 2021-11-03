434d0f21347aec696dd06bc3e6eb5e4da33fab03
DeAnna Boiani <deannaboiani@gmail.com>
2021-11-03 14:05:12 -0700

    nodemailer sends email to users on sign up

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const apiRoutes = require('./api')
 
-
+router.use("/api",apiRoutes)
+// router.use("/",frontEndRoutes)
+router.get("/sessions",(req,res)=>{
+    res.json(req.session)
+})
 
 module.exports = router;
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const sequelize = require("./config/connection.js")
 const session = require("express-session");
 const exphbs = require('express-handlebars');
-const nodemailer = require ('nodemailer')
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 // Sets up the Express App
 // =============================================================
@@ -12,8 +11,8 @@ const PORT = process.env.PORT || 3000;
 const hbs = exphbs.create({});
 
 // Requiring our models for syncing
-// const {User,Pet,Group} = require('./models');
-// const routes = require("./controllers");
+const {User,Artist,Post,Show} = require('./models');
+const routes = require("./controllers");
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -37,42 +36,11 @@ app.use(session({
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// 
 
-async function main() {
-    // Generate test SMTP service account from ethereal.email
-    // Only needed if you don't have a real mail account for testing
-    let testAccount = await nodemailer.createTestAccount();
-  
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: testAccount.user, // generated ethereal user
-        pass: testAccount.pass, // generated ethereal password
-      },
-    });
-  
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-      from: '"Fred Foo 👻" <foo@example.com>', // sender address
-      to: "bar@example.com, baz@example.com", // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>", // html body
-    });
-  
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-  
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-  }
-  
-  main().catch(console.error);
-// app.use(routes)
+  // nodemailer****
+
+app.use(routes)
 
 sequelize.sync({ force: false }).then(function() {
     app.listen(PORT, function() {
