ea89f925de0e1357d5b6bc57f2cb0417e6664eff
Vitaliy Trach <vitaliytrach@gmail.com>
2021-11-05 11:26:49 -0700

    Added post routes for the user post

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const { Post } = require("../../models");
+
+router.post("/", (req, res) => {
+    const date = new Date().toISOString().slice(0, 10);
+    const timestamp = new Date().toISOString().slice(0, 19).replace("T", " ");
+    
+    Post.create({
+        content: req.body.content,
+        post_date: date,
+        createdAt: timestamp,
+        updatedAt: timestamp,
+        UserId: req.body.userId,
+        ArtistId: req.body.ArtistId
+    })
+    .then(newPost => {
+        res.json(newPost);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({err});
+    });
+});
