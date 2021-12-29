86cd2a5717396b72bf525b0d92c43802fc145960
dimitermusic <dimiteryyordanov@gmail.com>
2021-12-29 14:16:42 -0800

    added delete artist feature

--- public/js/addPost.js
+++ public/js/addPost.js
@@ -1,23 +1,23 @@
 document.querySelector("#add-post").addEventListener("submit", (evt) => {
-    evt.preventDefault();
-    console.log(evt);
-    const fetchObj = {
-      // UserID:document.querySelector("#user-id").value,
-      ArtistId: document.querySelector("#artist-id").value,
-      content: document.querySelector("#content").value,
-    };
-    console.log(fetchObj);
-    fetch("/api/posts", {
-      method: "POST",
-      body: JSON.stringify(fetchObj),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).then((res) => {
-      if (res.ok) {
-        location.href = `/artists/${fetchObj.ArtistId}`;
-      } else {
-        alert("Post wasn't added");
-      }
-    });
+  evt.preventDefault();
+  console.log(evt);
+  const fetchObj = {
+    // UserID:document.querySelector("#user-id").value,
+    ArtistId: document.querySelector("#artist-id").value,
+    content: document.querySelector("#content").value,
+  };
+  console.log(fetchObj);
+  fetch("/api/posts", {
+    method: "POST",
+    body: JSON.stringify(fetchObj),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => {
+    if (res.ok) {
+      document.location.reload(true)
+    } else {
+      alert("Post wasn't added");
+    }
   });
+});
--- public/js/deletePost.js
+++ public/js/deletePost.js
@@ -1,16 +1,18 @@
-document.querySelector("#delete-post").addEventListener("click", (evt) => {
-    evt.preventDefault();
-    const postID = document.getElementById("postID").value;
-    fetch(`/api/posts/${postID}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).then((res) => {
-      if (res.ok) {
-        location.href = `/artists/${document.querySelector("#artist-id").value}`;
-      } else {
-        alert("Post wasn't deleted");
-      }
-    });
+const deletePost = document.querySelector("#delete-post");
+
+deletePost.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  const postID = document.getElementById("postID").value;
+  fetch(`/api/posts/${postID}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => {
+    if (res.ok) {
+      document.location.reload(true)
+    } else {
+      alert("Post wasn't deleted");
+    }
   });
+});
